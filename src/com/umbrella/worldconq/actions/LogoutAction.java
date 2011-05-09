ed92f58981c9e054e3e46507893ad066ecb78d8a
LauraN <arualitan@gmail.com>
2011-05-09 18:31:23 +0200

    logoutaction o algo parecido

---
+++ src/com/umbrella/worldconq/actions/LogoutAction.java
@@ -0,0 +1,41 @@
+package com.umbrella.worldconq.actions;
+
+import java.rmi.RemoteException;
+
+import exceptions.GameNotFoundException;
+import exceptions.InvalidSessionException;
+import exceptions.InvalidTimeException;
+import exceptions.NotCurrentPlayerGameException;
+
+public class LogoutAction extends WorldConqAction {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2251248368154290083L;
+
+	@Override
+	public String execute() {
+		try {
+			getApp().getUserManager().closeSession();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			this.addActionError("Error con el servidor remoto.");
+			session.remove("app");
+			return ERROR;
+		} catch (GameNotFoundException e) {
+			this.addActionError("Partida no encontrada.");
+			return ERROR;
+		} catch (InvalidSessionException e) {
+			this.addActionError("Sesión inválida");
+			return ERROR;
+		} catch (InvalidTimeException e) {
+			this.addActionError("Tiempo no válido");
+			return ERROR;
+		} catch (NotCurrentPlayerGameException e) {
+			this.addActionError("El jugador no esta en la partida");
+			return ERROR;
+		}
+		return SUCCESS;
+	}
+
+}
