ef1d37a800e97180a58e3a4eb90356886446383b
Ricardo Ruedas <ricardo.ruedas@gmail.com>
2011-06-02 18:25:48 +0200

    Cambiado GameEventPool con nueva interfaz y nuevos tipos de eventos.

--- src/com/umbrella/worldconq/domain/GameEventPool.java
+++ src/com/umbrella/worldconq/domain/GameEventPool.java
@@ -1,86 +1,70 @@
 package com.umbrella.worldconq.domain;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Vector;
 
 import com.umbrella.worldconq.ui.GameEventListener;
 
 import domain.Arsenal;
 import domain.Player;
 
-public class GameEventPool extends Vector<GameEvent> implements
-		GameEventListener {
+public class GameEventPool implements GameEventListener {
 
-	private static final long serialVersionUID = 3720667128671848534L;
+	ArrayList<GameEvent> pool;
+
+	public GameEventPool() {
+		pool = new ArrayList<GameEvent>();
+	}
 
 	@Override
 	public void territoryUnderAttack(TerritoryDecorator src, TerritoryDecorator dst, Arsenal arsenal) {
-		this.add(new GameEvent(EventType.TerritoryUnderAttackEvent,
-			"El territorio de " + dst.getName() + " está siendo atacado por "
-					+ src.getName() + "."));
+		pool.add(new GameEvent());
 	}
 
 	@Override
 	public void negotiationRequested(int money, int soldiers) {
-		this.add(new GameEvent(EventType.NegotiationEvent,
-			"Se está ofreciendo una negociación de " + Integer.toString(money)
-					+ " gallifantes y " + Integer.toString(soldiers)
-					+ " soldados."));
+		pool.add(new GameEvent(soldiers, money));
 	}
 
 	@Override
 	public void attackEvent(TerritoryDecorator src, TerritoryDecorator dst) {
-		this.add(new GameEvent(EventType.TerritoryUnderAttackEvent,
+		pool.add(new GameEvent(
 			"El territorio de " + dst.getName() + " está siendo atacado por "
 					+ src.getName() + "."));
 	}
 
 	@Override
 	public void negotiationEvent(TerritoryDecorator src, TerritoryDecorator dst) {
-		this.add(new GameEvent(EventType.NegotiationEvent,
+		pool.add(new GameEvent(
 			"Se está ofreciendo una negociación de " + src.getName()
 					+ " a " + dst.getName() + "."));
 	}
 
 	@Override
 	public void buyTerritoryEvent(TerritoryDecorator t) {
-		this.add(new GameEvent(EventType.BuyTerritoryEvent, t.getOwner()
-				+ " ha comprado " + t.getName()));
+		pool.add(new GameEvent(t.getOwner() + " ha comprado " + t.getName()));
 	}
 
 	@Override
 	public void buyUnitsEvent(TerritoryDecorator t) {
-		this.add(new GameEvent(EventType.BuyArsenalEvent,
+		pool.add(new GameEvent(
 			"Se ha realizado una compra de arsenal en " + t.getName()));
 	}
 
 	@Override
 	public void turnChangedEvent(Player p) {
-		this.add(new GameEvent(EventType.TurnChanged,
-			"Ha cambiado el turno. Es el turno de  " + p.getName()));
+		pool.add(new GameEvent("Ha cambiado el turno. Es el turno de  "
+				+ p.getName()));
 	}
 
 	@Override
 	public void winnerEvent(Player p) {
-		this.add(new GameEvent(EventType.WinnerEvent,
-			p.getName() + "ha ganado la partida."));
-	}
-
-	@Override
-	public Enumeration<GameEvent> elements() {
-		Enumeration<GameEvent> e = super.elements();
-		this.clear();
-		return e;
+		pool.add(new GameEvent(p.getName() + "ha ganado la partida."));
 	}
 
 	public ArrayList<GameEvent> getElements() {
-		Enumeration<GameEvent> e = this.elements();
-		ArrayList<GameEvent> listElements = new ArrayList<GameEvent>();
-		while (e.hasMoreElements())
-			listElements.add(e.nextElement());
-		return listElements;
-
+		ArrayList<GameEvent> events = this.pool;
+		this.pool = new ArrayList<GameEvent>();
+		return events;
 	}
 
 }
